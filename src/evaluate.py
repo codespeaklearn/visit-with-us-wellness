e7d33c99052f9dbc2879600517db96df2aa5ee63
codespeaklearn <codespeaklearn@gmail.com>
2026-02-04 20:01:36 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,12 +3,12 @@ import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("data/travel.csv")
+df = pd.read_csv("data/tourism.csv")
 
 X = df.drop("ProdTaken", axis=1)
 y = df["ProdTaken"]
 
-X = pd.get_dummies(X)
+X = pd.get_dummies(X)https://github.com/codespeaklearn/visit-with-us-wellness/edit/main/src/evaluate.py
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
